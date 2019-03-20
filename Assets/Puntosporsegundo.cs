4a634bef503e043adfa38e263ca963c177a70403
Pochu15 <Pochu15@users.noreply.github.com>
2019-03-20 21:13:09 +0100

    Puntos por segundo

---
+++ Assets/Puntosporsegundo.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Puntosporsegundo : MonoBehaviour
+{
+    public float puntos = 0f;
+
+    public Text variableText;
+
+    // Update is called once per frame
+    void Update()
+    {
+        puntos += Time.deltaTime;
+        variableText.text = "Puntuación : " + puntos;
+    }
+}
